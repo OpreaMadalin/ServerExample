1331204be612b677378a0a8fb0d02ab2d694defe
OpreaMadalin <madalinoprea1992@yahoo.com>
2021-04-07 19:41:10 +0300

    Commit

--- ExampleClient/src/com/company/client/ExampleClient.java
+++ ExampleClient/src/com/company/client/ExampleClient.java
@@ -19,12 +19,13 @@ public class ExampleClient {
 
     public void sendStdinForever() throws IOException {
         Socket socket = new Socket(this.ip, this.port);
+        System.out.println("Connected!");
         DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
         Scanner stdin = new Scanner(System.in);
         DataInputStream dis = new DataInputStream(socket.getInputStream());
 
         while (true) {
-            System.out.println("Connected!");
+
             System.out.println("Waiting for input...");
             String input = stdin.nextLine();
             System.out.println("Input received: " + input);
